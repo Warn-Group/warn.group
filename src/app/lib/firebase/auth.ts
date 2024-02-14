f4768d501ffa924f813da80eb7a9e5e9413e61e0
Paul MarisOUMary <paul.marisoumary@gmail.com>
2024-02-14 19:59:20 +0100

    add few firebase auth methods

--- src/app/lib/firebase/auth.ts
+++ src/app/lib/firebase/auth.ts
@@ -1,6 +1,18 @@
 import { firebase_auth } from "./config"
 
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signInAnonymously, signInWithPopup, AuthProvider, GoogleAuthProvider, signOut as logOut } from "firebase/auth";
+
+export async function signOut() {
+    let result = null,
+        error = null;
+    try {
+        result = await logOut(firebase_auth);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
 
 export async function signUp(email: string, password: string) {
     let result = null,
@@ -19,6 +31,38 @@ export async function signIn(email: string, password: string) {
         error = null;
     try {
         result = await signInWithEmailAndPassword(firebase_auth, email, password);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
+
+export async function signAnonymously() {
+    let result = null,
+        error = null;
+    try {
+        result = await signInAnonymously(firebase_auth);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
+
+export async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider()
+    provider.addScope('profile');
+    provider.addScope('email');
+
+    return signInWithProvider(provider);
+}
+
+async function signInWithProvider(provider: AuthProvider) {
+    let result = null,
+        error = null;
+    try {
+        result = await signInWithPopup(firebase_auth, provider);
     } catch (e) {
         error = e;
     }
