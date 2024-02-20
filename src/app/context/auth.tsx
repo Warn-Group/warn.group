eb22ad6c91cc0caed013fea02b6b569eda48d38a
Paul MarisOUMary <paul.marisoumary@gmail.com>
2024-02-20 22:22:38 +0100

    remove global import

--- src/app/context/auth.tsx
+++ src/app/context/auth.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { ReactNode } from 'react';
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { firebase_auth } from '../lib/firebase/config';
 
@@ -9,19 +9,19 @@ interface AuthContextValue {
     user?: User | undefined;
 }
 
-export const AuthContext = React.createContext<AuthContextValue>({});
+export const AuthContext = createContext<AuthContextValue>({});
 
-export const useAuthContext = (): AuthContextValue => { return React.useContext(AuthContext) };
+export const useAuthContext = (): AuthContextValue => { return useContext(AuthContext) };
 
 interface AuthContextProviderProps {
     children: ReactNode;
 }
 
 export const AuthContextProvider = ({ children } : AuthContextProviderProps) => {
-    const [user, setUser] = React.useState<User | undefined>(undefined);
-    const [loading, setLoading] = React.useState(true);
+    const [user, setUser] = useState<User | undefined>(undefined);
+    const [loading, setLoading] = useState(true);
 
-    React.useEffect(() => {
+    useEffect(() => {
         const unsubscribe = onAuthStateChanged(firebase_auth, (user) => {
             if (user) {
                 setUser(user);
@@ -34,7 +34,6 @@ export const AuthContextProvider = ({ children } : AuthContextProviderProps) => 
         return () => unsubscribe();
     }, []);
 
-    // Inject loading component bellow
     return (
         <AuthContext.Provider value={{ user }}>
             { loading ? <LoadingComp/> : children }
