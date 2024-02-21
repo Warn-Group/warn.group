ffbdd5957f59c391768bc1ecb2d441e7227f9252
Paul MarisOUMary <paul.marisoumary@gmail.com>
2024-02-21 18:11:33 +0100

    refactor addData

--- src/app/lib/firebase/firestore.ts
+++ src/app/lib/firebase/firestore.ts
@@ -2,12 +2,12 @@ import { firebase_firestore } from "./config"
 
 import { setDoc, addDoc, getDoc, getDocs, updateDoc, deleteDoc, doc, onSnapshot } from "firebase/firestore"
 
-export async function addData(coll: string, docu: string, data: any) {
-    let result = null;
-    let error = null;
+export async function addData(path: string, data: any) {
+    let result = null,
+        error = null;
 
     try {
-        result = await setDoc(doc(firebase_firestore, coll, docu), data, {
+        result = await setDoc(doc(firebase_firestore, path), data, {
             merge: true,
         });
     } catch (e) {
