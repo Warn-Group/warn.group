5a7b8f8077af1d670064405c5dae300dade8ed78
PaulMarisOUMary <paul.marisoumary@gmail.com>
2025-10-27 13:20:58 +0100

    Update dependencies and Tailwind/PostCSS config
    Upgraded major dependencies in package.json including Next.js, React, Firebase, TailwindCSS, and related packages. Updated Tailwind and PostCSS configuration files to support new versions. Adjusted SCSS files and TypeScript config for compatibility with updated tooling.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-
 import { nextui } from "@nextui-org/react";
 
 const config: Config = {
@@ -7,13 +6,13 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
   darkMode: "class",
-  plugins: [nextui({
-    defaultTheme: "dark",
-    addCommonColors: true,
-  })],
+  plugins: [
+    nextui({
+      defaultTheme: "dark",
+      addCommonColors: true,
+    })
+  ],
 };
+
 export default config;
