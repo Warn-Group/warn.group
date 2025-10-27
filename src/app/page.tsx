c6261540021a122871ae1797668f5a3aa58ef7b9
PaulMarisOUMary <paul.marisoumary@gmail.com>
2025-10-27 14:50:50 +0100

    Add home page hero section and improve responsiveness
    Introduces a new hero section to the home page with animated text and responsive styles. Updates header and spline component styles for better appearance and adaptability on smaller screens.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import './page.scss';
+
 export default function Root() {
   return (
-    <></>
+    <div className="home-container">
+      <div className="home-hero">
+        <h1 className="home-hero-text">
+          <span className="home-hero-line">CODE.</span>
+          <span className="home-hero-line">COMMIT.</span>
+          <span className="home-hero-line">COLLABORATE.</span>
+        </h1>
+      </div>
+    </div>
   );
 }
